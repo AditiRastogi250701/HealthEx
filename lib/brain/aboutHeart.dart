b70fd82918a8db1f2feb7f57fe8b0d285a8b8b24
Shresth Kapoor <shresthkapoor7@gmail.com>
2022-04-18 14:57:48 +0530

    Brain tumour

---
+++ lib/brain/aboutHeart.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:healthex/brain/FormHeart.dart';
+import 'package:healthex/brain/OverviewHeart.dart';
+import 'package:healthex/brain/SymptomsHeart.dart';
+import 'package:healthex/disease.dart';
+import 'package:healthex/subpages/Form.dart';
+import 'package:healthex/subpages/Overview.dart';
+import 'package:healthex/subpages/Symptoms.dart';
+
+class AboutHeart extends StatelessWidget {
+//   const
+// About({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        // ignore: prefer_const_literals_to_create_immutables
+        body: TabBarView(
+          children: [
+            OverviewHeart(),
+            SymptomsHeart(),
+            FormsHeart(),
+          ],
+        ),
+
+        appBar: AppBar(
+          bottom: TabBar(
+            tabs: [
+              Tab(
+                child: Text('Overview'),
+              ),
+              Tab(child: Text('Symptoms')),
+              Tab(
+                child: Text('Form'),
+              ),
+            ],
+          ),
+
+          backgroundColor: Colors.black,
+          // ignore: prefer_const_constructors
+          title: Text(
+            "Cardiovascular Disease",
+            style: TextStyle(
+              fontSize: 25,
+              fontStyle: FontStyle.italic,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
